0dfabd60089a0c85638dba93940ffb4aca4c9228
Fabian Raetz <fabian.raetz@gmail.com>
2019-12-03 11:34:25 +0100

    load tasks dynamically and start outputting individual stats

--- tools/update-bounty-tasks.js
+++ tools/update-bounty-tasks.js
@@ -2,19 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const _ = require('lodash');
-const fetchSpreadsheet = require('./lib/fetch-spreadsheet');
-const {camelize} = require('./lib/utils');
-
-function parseBountyTasks(rows) {
-    const headers = rows.shift().map(camelize);
-
-    return rows.map((row) => {
-        const bountyTask = _.zipObject(headers, row);
-        bountyTask.rules = bountyTask.rules.split('\n').filter(r => r !== '');
-        return bountyTask;
-    });
-}
+const fetchBountyTasks = require('./lib/fetch-bounty-tasks');
 
 function updateHarpData(bountyTasks) {
     const harpDataFile = path.join(__dirname, '../src/bounty/_data.json');
@@ -25,17 +13,11 @@ function updateHarpData(bountyTasks) {
     fs.writeFileSync(harpDataFile, JSON.stringify(harpData,null, 2));
 }
 
-
 (async () => {
    try {
-       const res = await fetchSpreadsheet({
-           spreadsheetId: '1t24TcE14RAVoCqxvIaid8xIxwYgRL2IKidkKllvOzJY',
-           range: 'Tasks',
-       });
-
-       const rows = res.data.values;
-       if (rows.length) {
-           const bountyTasks = parseBountyTasks(rows);
+       const bountyTasks = await fetchBountyTasks();
+       
+       if (bountyTasks.length) {
            updateHarpData(bountyTasks);
            console.log('Done.');
        } else {
